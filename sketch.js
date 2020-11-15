5a1e723a27c8b4021ecdaf647311de540d99993f
htmluiv <73861559+htmluiv@users.noreply.github.com>
2020-11-15 15:20:28 +0900

    drawing objects1

--- sketch.js
+++ sketch.js
@@ -1,8 +1,34 @@
+let p;
+
 function setup() {
   createCanvas(500, 500);
-
+  p = new Particle();
 }
 
 function draw() {
   background(100);
+  p.run();
+}
+
+class Particle  {
+constructor() {
+this.acc = createVector(0, 0.05);
+this.vel = createVector(random(-10, 10), random(-10, 10));
+this.pos = createVector(0,0);
+}
+
+run() {
+  this.update();
+  this.display();
+}
+
+update()  {
+  this.vel.add(this.acc);
+  this.pos.add(this.vel);
+}
+
+display() {
+  fill(255);
+  ellipse(100,100, 100, 100);
+}
 }
